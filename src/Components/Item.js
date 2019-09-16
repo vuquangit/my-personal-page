00f80680893cc578550d925e0f88c98e4b8be713
Quang <vuquangit@gmail.com>
2019-09-16 16:52:33 +0700

    Add Homepage component

--- src/Components/Item.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-export default class Item extends Component {
-  render() {
-    const { label } = this.props.data;
-
-    return <div className="child"> {label} </div>;
-  }
-}
--- src/Template/Pages/Home/index.js
+++ src/Template/Pages/Home/index.js
@@ -1,47 +1,10 @@
 import React from "react";
-
-const homeStyles = {
-  backgoundImage: {
-    width: "auto",
-    height: "100vh",
-    "background-position": "center",
-    "background-repeat": "no-repeat",
-    backgroundSize: "cover"
-  }
-};
+import HomePage from "../../../Components/HomePage";
 
 const Home = () => {
   return (
     <div>
-      <h1
-        style={{
-          color: "black",
-          textAlign: "center"
-        }}
-      >
-        WELLCOME TO MY HOMEPAGE
-      </h1>
-      <div
-        style={{
-          ...homeStyles.backgoundImage,
-          "background-image":
-            "url(https://www.apple.com/v/home/em/images/promos/apple-tv-plus/apple_tv_plus__euax5nw0l46e_large.jpg)"
-        }}
-      />
-      <div
-        style={{
-          ...homeStyles.backgoundImage,
-          "background-image":
-            "url(https://www.apple.com/v/home/em/images/heroes/ipad/ipad__bq6djchifrbm_large.jpg)"
-        }}
-      />
-      <div
-        style={{
-          ...homeStyles.backgoundImage,
-          "background-image":
-            "url(https://www.apple.com/v/home/em/images/heroes/watch-series-5/watch__csqqcayzqueu_large.jpg)"
-        }}
-      />
+      <HomePage />
     </div>
   );
 };
