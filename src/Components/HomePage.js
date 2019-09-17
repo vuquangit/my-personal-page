2fddc3d73f0c8d3e75a2f8144f31e1181536fc5e
Quang <vuquangit@gmail.com>
2019-09-17 20:00:34 +0700

    feat: modify homepage

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,15 +1,5 @@
 import React from "react";
 
-const homeStyles = {
-  backgoundImage: {
-    width: "auto",
-    height: "100vh",
-    "background-position": "center",
-    "background-repeat": "no-repeat",
-    backgroundSize: "cover"
-  }
-};
-
 const HomePage = () => {
   return (
     <div>
@@ -20,28 +10,7 @@ const HomePage = () => {
         }}
       >
         WELLCOME TO MY HOMEPAGE
-      </h1>
-      <div
-        style={{
-          ...homeStyles.backgoundImage,
-          "background-image":
-            "url(https://www.apple.com/v/home/em/images/promos/apple-tv-plus/apple_tv_plus__euax5nw0l46e_large.jpg)"
-        }}
-      />
-      <div
-        style={{
-          ...homeStyles.backgoundImage,
-          "background-image":
-            "url(https://www.apple.com/v/home/em/images/heroes/ipad/ipad__bq6djchifrbm_large.jpg)"
-        }}
-      />
-      <div
-        style={{
-          ...homeStyles.backgoundImage,
-          "background-image":
-            "url(https://www.apple.com/v/home/em/images/heroes/watch-series-5/watch__csqqcayzqueu_large.jpg)"
-        }}
-      />
+      </h1>     
     </div>
   );
 };
