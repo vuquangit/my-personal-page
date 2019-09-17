0064285ad55ebd7e1510be3be68bb338e33e5bb5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-17 14:25:52 +0000

    Deploy vuquangit/my-personal-page to github.com/vuquangit/my-personal-page.git:gh-pages

---
+++ precache-manifest.127ff877a5b440a8c8d9d0d2b777392b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54f161263aa30af4f12c189fdd199973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a512c27c4480419569c",
+    "url": "/static/css/main.30748d34.chunk.css"
+  },
+  {
+    "revision": "92b586258acd0f76524d",
+    "url": "/static/js/2.c23fc8be.chunk.js"
+  },
+  {
+    "revision": "3a512c27c4480419569c",
+    "url": "/static/js/main.fdc4f86d.chunk.js"
+  },
+  {
+    "revision": "d26c3395018d257f4e98",
+    "url": "/static/js/runtime~main.830ddce1.js"
+  }
+]);
